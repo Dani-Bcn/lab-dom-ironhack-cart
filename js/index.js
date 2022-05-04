a052b33829f219fbcf89c40418c3b71044520a8a
Dani <nneodani@gmail.com>
2022-05-04 17:43:26 +0200

    a falta de iteración 5

--- js/index.js
+++ js/index.js
@@ -1,42 +1,53 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = (parseInt(product.querySelector('.price span').innerHTML))
+  const quantity = parseInt(product.querySelector('input').value)
+  let subTotal = price * quantity
+  product.querySelector(".subtotal span").innerHTML=subTotal
+  console.log(product)
 }
-
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
+ // ITERATION 2  
+const [...products] = document.getElementsByClassName("product")
+let prices=0
+function calculateAll() {   
+    products.forEach((element)=>{   
+      updateSubtotal(element)
+      prices = prices += parseInt(element.querySelector(".subtotal span").innerHTML)
+    })
   // ITERATION 3
-  //... your code goes here
+  document.getElementById("total-value").innerHTML=`Total: $${prices}`
 }
-
 // ITERATION 4
-
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
-
+products.forEach(removeProduct =element=>{
+  element.querySelector(".product .btn").addEventListener("click", ()=>{
+  element.remove("tr")     
+  prices = prices -= parseInt(element.querySelector(".subtotal span").innerHTML)
+  document.getElementById("total-value").innerHTML=`Total: $${prices}`
+  }) 
+})
 // ITERATION 5
-
-function createProduct() {
-  //... your code goes here
-}
-
+document.getElementById("create").addEventListener("click", ()=>{
+    let createTr=document.createElement("tr")
+    createTr.className="product"    
+    let newName=document.getElementById("newname").value
+    let newPrice=document.getElementById("newprice").value
+  createTr.innerHTML=`  
+                        <td class="name"><span>${newName}</span></td>
+                        <td class="price">$<span>25.00</span></td>
+                        <td class="quantity">
+                        <input type="number" value=5 min="0" placeholder="Quantity" />
+                        </td>
+                         <td class="subtotal">$<span>0</span></td>
+                        <td class="action">
+                        <button class="btn btn-remove"> Remove</button>
+                        </td>
+                        <tr>
+                        `
+  document.querySelector("tbody").appendChild(createTr)
+   updateSubtotal(createTr)
+})
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  calculatePricesBtn.addEventListener('click', calculateAll); 
 });
+
